3e2c8b8f629b3f0f32cc004c7ebbd5017dc48661
Ishan Rijal <ishanrijal6@gmail.com>
2020-05-15 01:18:22 +0545

    Updates

---
+++ precache-manifest.18ae864aedcc526604f2a36ce5b506c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe48e97bb4137acc6a3887c5ea381b5",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "f5807228a60e39ccb0a9",
+    "url": "/covid-19/static/css/main.eff8d3ef.chunk.css"
+  },
+  {
+    "revision": "1cb5a64522a367622d51",
+    "url": "/covid-19/static/js/2.2fab5bfa.chunk.js"
+  },
+  {
+    "revision": "5f7f07d03df7b32bac4478735c79348f",
+    "url": "/covid-19/static/js/2.2fab5bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5807228a60e39ccb0a9",
+    "url": "/covid-19/static/js/main.cb877790.chunk.js"
+  },
+  {
+    "revision": "c59e2f922549c307064d",
+    "url": "/covid-19/static/js/runtime-main.aabceb11.js"
+  }
+]);
